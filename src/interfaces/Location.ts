3d5d770ac90343df55f8314d54c9a4a0f64f9cbe
sousa-p <p.soousa7@gmail.com>
2024-09-26 23:00:35 -0300

    :rocket: feat: Location.ts

---
+++ src/interfaces/Location.ts
@@ -0,0 +1,11 @@
+interface Location {
+    id: number,
+    name: string,
+    type: string,
+    dimension: string,
+    residents: string[],
+    url: string,
+    created: string,
+};
+
+export default Location;
