7f495432d25d9834cbabb0b7abe0f84eaee84fae
sousa-p <p.soousa7@gmail.com>
2024-09-26 23:00:16 -0300

    :rocket: feat: Episode.ts

---
+++ src/interfaces/Episode.ts
@@ -0,0 +1,11 @@
+interface Episode {
+    id: number,
+    name: string,
+    air_date: string,
+    episode: string,
+    characters: string[],
+    url: string,
+    created: string,
+};
+
+export default Episode;
