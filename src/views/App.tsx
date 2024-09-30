e85ba5e929a559a6c94969b82f439732d678f9fa
Pedro S. <97417230+sousa-p@users.noreply.github.com>
2024-09-30 17:48:57 +0000

    :building_construction: arqs: todas páginas

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,10 +1,18 @@
 import { setupIonicReact } from "@ionic/react";
 import "./App.css";
 import "@ionic/react/css/core.css";
+import { useEffect } from "react";
+import { CharacterService } from "../services/characters/characterService";
 
 setupIonicReact();
 
+
+
 function App() {
+  useEffect(() => {
+    CharacterService.getAll();
+  }, []);
+
   return (
     <>
       <h1>Conteúdo</h1>
