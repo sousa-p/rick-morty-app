ae89cab0fb3948931abbb4716053040b283628aa
sousa-p <p.soousa7@gmail.com>
2024-10-06 20:18:25 -0300

    :rocket: feat: about page

--- src/views/pages/AboutPage/AboutPage.tsx
+++ src/views/pages/AboutPage/AboutPage.tsx
@@ -1,13 +1,68 @@
-import { IonCol, IonContent, IonGrid, IonRow } from "@ionic/react";
+import {
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonText,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
 import SlidesComponent from "./components/SlidesComponent/SlidesComponent";
+import OpenYourEyesMorty from "../../../assets/openYourEyesMorty.png";
 
 function AboutPage() {
   return (
     <IonContent>
       <IonGrid>
-        <IonCol size="12">
-          <SlidesComponent></SlidesComponent>
-        </IonCol>
+        <IonRow class="ion-justify-content-center">
+          <IonCol size="12" sizeMd="10" sizeLg="9">
+            <IonToolbar>
+              <IonTitle size="large">A little about Rick and Morty</IonTitle>
+            </IonToolbar>
+            <IonRow>
+              <IonText class="ion-margin ion-text-justify">
+                The show centers on the Smith family: parents Jerry and Beth,
+                their children Summer and Morty, and Beth's father, Rick
+                Sanchez, who lives with them. According to co-creator Justin
+                Roiland, the family resides near Seattle, Washington. However,
+                Rick and Morty's adventures span across infinite realities,
+                using portals and Rick's flying vehicle to travel to other
+                planets and dimensions.
+              </IonText>
+            </IonRow>
+            <SlidesComponent></SlidesComponent>
+
+            <IonRow>
+              <IonText class="ion-margin ion-text-justify">
+                Rick, an eccentric and alcoholic mad scientist, rejects societal
+                norms like school, love, and family. He often takes his
+                14-year-old grandson Morty, a well-meaning but easily
+                overwhelmed boy, on dangerous and bizarre adventures. Morty's
+                moral compass clashes with Rick's selfish behavior. Summer,
+                Morty's 17-year-old sister, is a typical teenager focused on
+                social status, but occasionally joins their adventures. Beth,
+                their mother, balances her professional life as a horse surgeon
+                with being a strong, assertive figure at home, though she is
+                unsatisfied with her marriage to Jerry, who frequently
+                criticizes Rick’s influence on their family.
+              </IonText>
+            </IonRow>
+
+            <IonRow>
+              <IonText class="ion-margin ion-text-justify">
+                The characters have alternate versions across the multiverse,
+                and their personalities can vary in different realities. Rick
+                identifies as "Rick Sanchez of Earth dimension C-137." After an
+                experiment goes wrong in the episode "Rick Potion #9," Rick and
+                Morty leave their original dimension behind, moving to a
+                parallel world and leaving their old family to deal with the
+                consequences.
+              </IonText>
+            </IonRow>
+
+            <img src={OpenYourEyesMorty} style={{ height: "250px" }} />
+          </IonCol>
+        </IonRow>
       </IonGrid>
     </IonContent>
   );
--- src/views/pages/AboutPage/components/SlidesComponent/SlidesComponent.tsx
+++ src/views/pages/AboutPage/components/SlidesComponent/SlidesComponent.tsx
@@ -1,25 +1,29 @@
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
+import "./SlidesComponent.css";
+
+import Slide1 from "../../../../../assets/slides/slide1.png";
+import Slide2 from "../../../../../assets/slides/slide2.png";
+import Slide3 from "../../../../../assets/slides/slide3.png";
+import Slide4 from "../../../../../assets/slides/slide4.png";
+import Slide5 from "../../../../../assets/slides/slide5.png";
+import Slide6 from "../../../../../assets/slides/slide6.png";
+import Slide7 from "../../../../../assets/slides/slide7.png";
+
+const images = [Slide1, Slide2, Slide3, Slide4, Slide5, Slide6, Slide7];
 
 const SlideComponent = () => {
-  const images = [
-    "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-    "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80",
-    "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
-  ];
-
   return (
     <Slide>
-      <div className="each-slide-effect">
-        <img src={images[0]} alt="" />
-      </div>
-      <div className="each-slide-effect">
-        <img src={images[0]} alt="" />
-      </div>
-      <div className="each-slide-effect">
-        <img src={images[0]} alt="" />
-      </div>
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className="each-slide-effect"
+          style={{ backgroundImage: `url(${image})` }}
+        ></div>
+      ))}
     </Slide>
   );
 };
+
 export default SlideComponent;
